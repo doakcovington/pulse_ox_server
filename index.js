99a7b78a6a10e7355d247877b32b0f5b925679e0
doakcovington <doakcovington@protonmail.com>
2020-08-18 14:43:46 -0700

    Built out create route

--- index.js
+++ index.js
@@ -2,6 +2,31 @@
 //NCWH
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const pool = require('./database')
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+
+//create record
+app.post("/records", async (request, response) => {
+    try {
+        const date = request.body.date;
+        const oxygen = request.body.oxygen;
+        const newRecord = await pool.query("INSERT INTO reports (date, oxygen) VALUES($1, $2)", [date, oxygen]);
+        response.json(newRecord);
+    } catch (error) {
+        console.log(error.message)
+    }
+});
+//get all records
+
+//get a record
+
+//delete a record
 
 app.listen(5000, () => {
   console.log("We Don't Go To Ravenholm");  
